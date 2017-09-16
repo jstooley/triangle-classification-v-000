5e667264b537afe3a96e1dd19b6a1296271ed353
Justin Tooley <jstooley823@gmail.com>
2017-09-16 08:13:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,7 @@ class Triangle
   end
 
   def equilateral?
-    if self.side_1 == self.side_2 && self.side_2 == self.side_3
-      return true
-    else
-      false
-    end
+    self.side_1 == self.side_2 && self.side_2 == self.side_3
   end
 
   def isosceles?
